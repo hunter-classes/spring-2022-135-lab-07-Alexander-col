e6e033d1cf4c92f650e40f66ae617ad9ba6ba465
Alexander-col <aexro2221@gmail.com>
2022-03-17 15:02:38 -0400

    udpated task A

--- funcs.cpp
+++ funcs.cpp
@@ -7,8 +7,18 @@ std::string removeLeadingSpaces(std::string str)
     int sl = str.length();
     for (int i = 0; i != sl; i++)
     {
-        if (str[i] != ' ')
+        if (str[i] != ' ' )
         output += str[i];
+
+
+        // if (str[i+1] != ' ' || str[i-1] != ' ')
+        // {
+        //     output += str[i];
+        // }
+        // else if (str[i] == ' ')
+        // {
+        // output -= str[i];
+        // }
     }
 
     return output;
