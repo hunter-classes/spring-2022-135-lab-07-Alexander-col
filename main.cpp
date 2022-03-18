19ab8b803dea82a41a4a36b021111420bda72efc
Alexander-col <aexro2221@gmail.com>
2022-03-18 00:29:30 -0400

    main was updated to print out the basic broken code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
-#include "funcs.h"
-
+#include <fstream>
+#include "unindent.h"
 
 int main()
 {
+    std::string line;
+    int out_brace = 0;
+    int in_brace = 0; 
     std::cout << removeLeadingSpaces("                int main(){") << std::endl;
+
+	std::string t_line;
+
+	std::ifstream bad_code("bad-code.cpp");
+	while (getline(bad_code, t_line))
+	{
+		std::cout << t_line << std::endl;
+	}
+
+		while (getline(bad_code, t_line))
+	{
+		std::cout << removeLeadingSpaces(t_line) << std::endl;
+	}
+
+
+
+
+
+    return 0;
 }
+
+
+
+
+    // std::fstream in("bad-code.cpp", std::ios_base::in);
+
+	// ifstream inFile;
+
+	// inFile.open("hello.txt", ios::in);
+	// string output = "";
+	// char c = ' ';
+	// while (inFile.get(c))
+    // {
+	// 	output = output + c;
+    // }
+	// cout << output << endl;
+	// return 0;
